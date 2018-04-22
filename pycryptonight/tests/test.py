9846aa667d080bf4eeaf6606e6828d1603402cbc
Dusan Klinec <dusan.klinec@gmail.com>
2018-04-22 02:56:49 +0200

    tests added

---
+++ pycryptonight/tests/test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import random
+import binascii
+import unittest
+import pkg_resources
+import pycryptonight
+
+__author__ = 'dusanklinec'
+
+
+class PyCryptoNightTest(unittest.TestCase):
+
+    def test_fast(self):
+        self.tst_method('tests-fast.txt', lambda x: pycryptonight.cn_fast_hash(x))
+
+    def test_slow(self):
+        self.tst_method('tests-slow.txt', lambda x: pycryptonight.cn_slow_hash(x))
+
+    def test_slow1(self):
+        self.tst_method('tests-slow-1.txt', lambda x: pycryptonight.cn_slow_hash(x, 1))
+
+    def tst_method(self, fname, fnc):
+        data_file = pkg_resources.resource_stream(__name__, os.path.join('data', fname))
+        for idx, line in enumerate(data_file):
+            line = line.strip()
+            if line == '':
+                continue
+
+            expected, inp = line.split(' ', 2)
+            res = fnc(binascii.unhexlify(inp))
+            self.assertEqual(binascii.unhexlify(expected), res)
+
+
+if __name__ == "__main__":
+    unittest.main()  # pragma: no cover
+
+
