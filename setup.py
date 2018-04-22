85c509a491183b2c78d16f163599367c9135b95d
Dusan Klinec <dusan.klinec@gmail.com>
2018-04-22 03:05:56 +0200

    setyp.py - py2.7

--- setup.py
+++ setup.py
@@ -82,6 +82,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Security',
