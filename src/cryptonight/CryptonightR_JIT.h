67309f12118fe8cb00964d5a54e7e7b1db0a87d9
Dusan Klinec <dusan.klinec@gmail.com>
2020-01-06 00:44:25 +0100

    gcc-only macro fix

--- src/cryptonight/CryptonightR_JIT.h
+++ src/cryptonight/CryptonightR_JIT.h
@@ -9,8 +9,13 @@
 // - Allocate writable and executable memory
 // - Call v4_generate_JIT_code with "buf" pointed to memory allocated on previous step
 // - Call the generated code instead of "v4_random_math(code, r)", omit the "code" parameter
+#if (defined(__GNUC__) || defined(__clang__))
+#define ATTR_ABI __attribute__((sysv_abi))
+#else
+#define ATTR_ABI
+#endif
 
-typedef void (*v4_random_math_JIT_func)(uint32_t* r) __attribute__((sysv_abi));
+typedef void (*v4_random_math_JIT_func)(uint32_t* r)ATTR_ABI;
 
 // Given the random math sequence, generates machine code (x86-64) for it
 // Returns 0 if code was generated successfully
